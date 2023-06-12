82e7829ca3d39f2192d2c85b45de1df4d99d5a05
7AiRzzZ <maxencedpr@icloud.com>
2023-06-12 08:34:47 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,39 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Salut les gens!</h1>
-      
-    </main>
-  )
+    <div>
+      <body>
+        <header>
+          <img
+            src="https://cdn.shopify.com/s/files/1/2693/0654/articles/pourquoi-kakashi-sharingan_b794263b-5363-4f24-a05b-70dc73ac884d.jpg?v=1575286554&width=1500"
+            alt="sharingan kakashi"
+          />
+          <nav>
+            <ul>
+              <li className="italic text-red-500">
+                <a href="#">accueil</a>
+              </li>
+              <li>
+                <a href="#">contact</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        <main className="bg-grey-500">
+          <h1>Petit test </h1>
+          <p>
+            nzxzzdddzdazdd dadazdazdazd azdadazdazda dzdadazdazd azdazdzadaz
+            dazdzadzadzad azdazdz
+          </p>
+        </main>
+        <footer>
+          <img
+            src="https://logo-marque.com/wp-content/uploads/2021/08/Among-Us-Logo.png"
+            alt="among us"
+          />
+        </footer>
+      </body>
+    </div>
+  );
 }
