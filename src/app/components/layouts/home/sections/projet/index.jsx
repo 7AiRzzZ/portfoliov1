d0450dc4aabd2dbf070f015b052e4c410b8c1b84
7AiRzzZ <maxencedpr@icloud.com>
2023-07-26 16:38:40 +0200

    update

--- src/app/components/layouts/home/sections/projet/index.jsx
+++ src/app/components/layouts/home/sections/projet/index.jsx
@@ -11,14 +11,14 @@ const Projet = () => {
         <h1>Individuel:</h1>
       </div>
       <div className="grid grid-cols ">
-        <div className=" mx-4  backdrop-blur-sm bg-green-400/30  flex flex-row text-xs text-center items-center py-2 px-2 border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg">
+      <div className=" mx-14 my-2  backdrop-blur-sm bg-green-400/30  flex flex-row text-xs items-center py-2 px-2 border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg">
           <Image
             src="/Images/ayanokoji.png"
             width={50}
             height={25}
             alt="Maxence Dupre"
           />
-          <div className="pl-4">
+          <div className="px-2">
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero
               optio aperiam facere? Dolorum natus eveniet nulla commodi atque
@@ -27,7 +27,7 @@ const Projet = () => {
             </p>
           </div>
         </div>
-        <div className=" mx-4 mt-4  backdrop-blur-sm bg-green-400/30  flex flex-row text-xs items-center py-2 px-2 border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg">
+        <div className=" mx-14 my-2  backdrop-blur-sm bg-green-400/30  flex flex-row text-xs items-center py-2 px-2 border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg">
           <Image
             src="/Images/ayanokoji.png"
             width={50}
@@ -48,14 +48,14 @@ const Projet = () => {
         <h1>Collaboration:</h1>
       </div>
       <div className="grid grid-cols ">
-        <div className=" mx-4  backdrop-blur-sm bg-green-400/30  flex flex-row text-xs text-center items-center py-2 px-2 border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg">
+      <div className=" mx-14 my-2 backdrop-blur-sm bg-green-400/30  flex flex-row text-xs items-center py-2 px-2 border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg">
           <Image
             src="/Images/ayanokoji.png"
             width={50}
             height={25}
             alt="Maxence Dupre"
           />
-          <div className="pl-4">
+          <div className="px-2">
             <p>
               Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero
               optio aperiam facere? Dolorum natus eveniet nulla commodi atque
@@ -64,7 +64,7 @@ const Projet = () => {
             </p>
           </div>
         </div>
-        <div className=" mx-4 mt-4  backdrop-blur-sm bg-green-400/30  flex flex-row text-xs items-center py-2 px-2 border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg">
+        <div className=" mx-14 my-2  backdrop-blur-sm bg-green-400/30  flex flex-row text-xs items-center py-2 px-2 border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg">
           <Image
             src="/Images/ayanokoji.png"
             width={50}
