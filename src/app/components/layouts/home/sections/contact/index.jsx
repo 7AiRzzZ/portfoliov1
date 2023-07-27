74886aeb2276b1919c33e7453cbfe8d55d005132
7AiRzzZ <maxencedpr@icloud.com>
2023-07-27 15:20:29 +0200

    update

--- src/app/components/layouts/home/sections/contact/index.jsx
+++ src/app/components/layouts/home/sections/contact/index.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
         <Btn text={"Contact"} />
       </div>
 
-      <div className=" backdrop-blur-sm bg-green-400/30  border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg flex flex-row justify-center mx-2 mb-2 pt-4">
+      <div className=" backdrop-blur-sm bg-green-400/30  border-t-8 border-t-green-700 border-l-8 border-l-green-700 border-r-8 border-r-white border-b-8 border-b-white rounded-sm shadow-lg flex flex-row justify-center mx-2 mb-4 pt-4">
         <form action="">
           <div className=" flex flex-row justify-center items-center align-middle">
             <label htmlFor="">
@@ -38,7 +38,7 @@ const Contact = () => {
             <label htmlFor="">
               <input
                 type="text"
-                className=" text-xs backdrop-blur-sm bg-gray-200/10   border-t-4 border-t-green-700 border-l-4 border-l-green-700 border-r-4 border-r-green-700 border-b-4 border-b-green-700 rounded-sm shadow-lgbg-red-500mx-5 mb-5 w-20 h-10"
+                className=" text-xs backdrop-blur-sm bg-gray-200/10 border-t-4 border-t-green-700 border-l-4 border-l-green-700 border-r-4 border-r-green-700 border-b-4 border-b-green-700 rounded-sm shadow-lgbg-red-500mx-5 mb-5 w-20 h-10"
                 placeholder="    Envoyer"
               />
             </label>
