8285334a630e8aafe7117a3990fb3b997ae80757
7AiRzzZ <maxencedpr@icloud.com>
2023-08-09 09:26:19 +0200

    update

--- src/app/components/layouts/footer/index.jsx
+++ src/app/components/layouts/footer/index.jsx
@@ -1,9 +1,11 @@
 // import FooterDesktop from "@/app/components/layouts/footer/desktop";
 // import FooterMobile from "@/app/components/layouts/footer/mobile";
+import FooterMobile from "./mobile";
 
 const Footer = () => {
   return (
     <>
+      <FooterMobile />
       {/* <FooterMobile /> */}
       {/* <FooterDesktop /> */}
       <h1>footer</h1>
